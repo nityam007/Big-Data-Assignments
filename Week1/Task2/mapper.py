d4368bca2718c6dc524f589f65ffa8a200b595c6
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-09-14 22:56:55 +0530

    added code with random variables haha

--- Week1/Task2/mapper.py
+++ Week1/Task2/mapper.py
@@ -1,20 +1,18 @@
-import json
-import sys
-from datetime import datetime
-
-import requests
-
-URL='http://20.185.44.219:5000/'
-
-Start_Lat=sys.argv[1]
-Start_Lng=sys.argv[2]
-#D=sys.argv[3]
-#print(Start_Lat)
-n = len(sys.argv)
-myojb={"latitude": Start_Lat,
-    "longitude": Start_Lng}
-r=requests.post( url=URL, json=myojb)
-data=r.json()
-
-    
-print(data)
+#!/usr/bin/env python3
+import sys, json,requests
+lat=float(sys.argv[1])
+lng=float(sys.argv[2])
+d=float(sys.argv[3])
+a=0
+b=0
+for line in sys.stdin:
+    line=line.strip()
+    res=json.loads(line)
+    start_lat=float(res['Start_Lat'])
+    start_lng=float(res['Start_Lng'])
+    ed=((start_lat-lat)*2 + (start_lng-lng)2)*0.5
+    if(float(ed)<d):
+        payload={"latitude":start_lat,"longitude":start_lng}
+        re=requests.post(url="http://20.185.44.219:5000/",json=payload)
+        ev=re.json()
+        print(ev['city'],ev['state'],1)
