c7f93d22cdf8dbe7915f790feec4bb898e9a99e8
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-10-30 17:15:54 +0530

    added city count greater than avg temp
    need to change the path to command line arg, and need to take care of NaN values

--- A3/task1.py
+++ A3/task1.py
@@ -1,10 +1,11 @@
 import csv
+import sys
 
-
-country='India'
+country=sys.argv[1]
+#print(sys.argv[0])
 path='/Users/Darshilshah/Downloads/city_sample_5percent.csv'
-city_vs_avgtemp=dict()
-city_count=dict()
+city_gtt_avgtemp=dict()
+city_avgtemp=dict()
 file=open(path,"r")
 data=file.read()
 i=0
@@ -15,24 +16,36 @@ with open(path) as csv_file:
     for line in csv_reader:
         if(line[4]==country):
             
-            if(line[3] in city_count):
+            if(line[3] in city_avgtemp):
                 try:
-                    city_count[line[3]]+=float(line[1])
+                    city_avgtemp[line[3]]+=float(line[1])
                 except:
-                    #print(line)
-                    i+=1
-                
+                    i+=1          
             
             else:
                 try:
-                    city_count[line[3]]=float(line[1])
+                    city_avgtemp[line[3]]=float(line[1])
                 except:
-                    #print(line)
                     i+=1
                 
-    for x in city_count:
+    for x in city_avgtemp:
          occurences=data.count(x)
-         city_count[x]=city_count[x]/occurences
-    print(city_count)
+         city_avgtemp[x]=city_avgtemp[x]/occurences
+            
 
-print(i)
+
+with open(path) as csv_file2:
+    csv_reader=csv.reader(csv_file2)
+    for line2 in csv_reader:
+        if(line2[4]==country): 
+            try:
+                if((float(line2[1])>city_avgtemp[line2[3]])):
+                    city_gtt_avgtemp[line2[3]]+=1
+                    
+                else:
+                    city_gtt_avgtemp[line2[3]]=1
+            except:
+                i+=1          
+            
+
+    print(city_gtt_avgtemp)
