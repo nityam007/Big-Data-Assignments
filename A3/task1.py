8295afc6d2116db230e12cc21c951dd360da5c69
Nityam Churamani <68228031+nityam007@users.noreply.github.com>
2021-10-31 16:27:13 +0530

    The final task1 using spark
    My code is present in the comments below

--- A3/task1.py
+++ A3/task1.py
@@ -49,3 +49,45 @@ with open(path) as csv_file2:
             
 
     print(city_gtt_avgtemp)
+ 
+'''from pyspark.sql import SparkSession
+import sys
+
+spark = SparkSession.builder.appName('A3T1').getOrCreate()
+
+hold = spark.read.csv("C:/Users/Nityam/Dropbox (Old)/My PC (DESKTOP-P3UAN3I)/Downloads/city_sample.csv", sep=',',inferSchema=True, header=True)
+df = hold.toPandas()
+
+cities=[]
+for i in df.iloc():
+    if(i['Country']=='Germany'):
+        if(i['City'] not in cities):
+            cities.append(i['City'])
+            
+cities.sort() #Sorting all the cities corresponding to a country in lexiographical order
+#print(cities)
+
+avg_temp={} #Stores the City and its average temperature as key-value pairs
+
+for i in cities:
+    sum_val=0
+    count=0
+    for j in df.iloc:
+        if (j['City']==i):
+            sum_val=sum_val+j['AverageTemperature']
+            count+=1
+    
+    avg_temp[i]=(sum_val/count)
+
+for k in avg_temp:
+    count=0
+    for p in df.iloc():
+        if p['City']==k:
+            if(p['AverageTemperature']>avg_temp[k]):
+                count+=1
+    
+    if(count>=1):
+        print(k,end="")
+        print("\t",end="")
+        print(count)  
+ '''
