3a1c274103f34aa2a01d0b04da3c0ce821c1eb9c
Nityam Churamani <68228031+nityam007@users.noreply.github.com>
2021-11-02 13:49:12 +0530

    Final Task 1 code

--- A3/task1.py
+++ A3/task1.py
@@ -4,59 +4,36 @@ import sys
 from functools import reduce
 from pyspark.sql.types import *
 
-
-path1=sys.argv[1]
-path2=sys.argv[2]
+country=sys.argv[1]
+path=sys.argv[2]
 
 spark = SparkSession.builder.appName('A3T2').getOrCreate()
 
 
-df_1=spark.read.option("header","true").csv(path1)
-df_2=spark.read.option("header","true").csv(path2)
 
-df_1 = df_1.withColumn("AverageTemperature",df_1.AverageTemperature.cast('float')) 
-df_2 = df_2.withColumn("LandAverageTemperature",df_2.LandAverageTemperature.cast('float')) 
+df=spark.read.option("header","true").csv(path)
 
-df_1=df_1.withColumnRenamed("dt","date")
+df_only_country=df.where(df.Country==country)
 
-df_only_join=df_1.join(df_2,df_1.date==df_2.dt)
+df2= df_only_country.withColumn("AverageTemperature",col("AverageTemperature").cast(FloatType()))
 
+df3=df2.groupBy("City").mean("AverageTemperature")
 
-rdd_1=df_only_join
-#rdd_1.show()
-#rdd_1.groupBy('date','AverageTemperature','Country').count().show()
+oldColumns = df3.schema.names
 
-#dropDisDF = df.dropDuplicates(["department","salary"])
+newColumns=["ew","cityavgtemp"]
 
-rdd_3 = rdd_1.groupBy('Country','date').max('AverageTemperature')
-#rdd_3.show()
+temp= reduce(lambda df3, idx: df3.withColumnRenamed(oldColumns[idx], newColumns[idx]), range(len(oldColumns)), df3)
 
-rdd_3=rdd_3.withColumnRenamed("Country","Country_rdd3")
-rdd_3=rdd_3.withColumnRenamed("date","date_rdd3")
-#rdd_3=rdd_3.withColumnRenamed("City","City_rdd3")
-rdd_3=rdd_3.withColumnRenamed("max(AverageTemperature)","max_avg_temp")
+joined_df=df2.join(temp,df2.City==temp.ew)
 
-#rdd_3=rdd_3.dropDuplicates(['Country_rdd3',"max_avg_temp"])
+final_res=joined_df.filter(joined_df.AverageTemperature>joined_df.cityavgtemp).groupBy("City").count()
 
-#rdd_3.show()
+final_res=final_res.orderBy(['City'])
 
+rdd_final_vals=final_res.collect()
 
-
-rdd_new=rdd_3.join(rdd_1,(rdd_1.date==rdd_3.date_rdd3) & (rdd_1.Country==rdd_3.Country_rdd3) & (rdd_1.AverageTemperature==rdd_3.max_avg_temp) )
-rdd_new=rdd_new.dropDuplicates(['Country_rdd3','date_rdd3','max_avg_temp'])
-
-#rdd_new.show()
-
-rdd_new=rdd_new.filter(rdd_new['max_avg_temp'] > rdd_new['LandAverageTemperature'])
-#rdd_new.show()
-rdd_new=rdd_new.groupBy('Country').count()
-rdd_new=rdd_new.orderBy(['Country'])
-rdd_new=rdd_new.collect()
-
-#rdd_new=rdd_new.groupBy('date_rdd3','Country').count()
-#rdd_new.show()
-
-for i in rdd_new:
-    print(i['Country'],end='')
-    print("\t",end='')
+for i in rdd_final_vals:
+    print(i['City'],end="")
+    print("\t",end="")
     print(i['count'])
